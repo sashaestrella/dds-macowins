2d60c2fcb9c9ffeff5bcaa71ae7b4440f9c23278
Sasha Belen Estrella <sestrella@lanacion.com.ar>
2022-04-07 21:41:59 -0300

    termino ejercicio y agrego diagrama de clases

--- src/main/java/TipoDePrenda.java
+++ src/main/java/TipoDePrenda.java
@@ -1,3 +1,7 @@
+/*por el momento considero a TipoDePrenda como un Enum, si después
+* se agregara un estado y comportamiento distinto por cada uno
+* TipoDePrenda podria ser una interface y los tipos de prenda
+* subclases de esta*/
 public enum TipoDePrenda {
     CAMISA,
     SACO,
